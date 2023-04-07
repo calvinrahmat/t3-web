293bd20a5f276e3ff9e5bdb5fdef1037347b6733
Calvin <calvin.rahmat@gmail.com>
2023-04-07 16:26:01 +0700

    Add Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
 
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -20,6 +20,11 @@ const Home: NextPage = () => {
         <div>
           {!user && <SignInButton />}
           {!!user && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
       </main>
